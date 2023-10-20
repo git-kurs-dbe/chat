cf149ad5ccd4b54c334adb825c4ff162aa6020da
git-kurs-dbe <git-kurs@dbe.academy>
2023-10-20 12:01:19 +0200

    erstellung der datei job.php
    ich fang einfach mal an

---
+++ job.php
@@ -0,0 +1,22 @@
+<?php
+$lockFile = 'script.lock';
+
+// Überprüfen, ob das Skript bereits ausgeführt wird
+if (file_exists($lockFile)) {
+    die("Das Skript wird bereits ausgeführt");
+}
+
+// Sperren der Datei für andere Skripte
+$handle = fopen($lockFile, 'w');
+if (!flock($handle, LOCK_EX | LOCK_NB)) {
+    die("Das Skript wird bereits ausgeführt");
+}
+
+// Hier kannst du den Code deines Skripts einfügen
+// ...
+
+// Freigeben der Sperre und Schließen der Datei
+flock($handle, LOCK_UN);
+fclose($handle);
+unlink($lockFile);
+?>
